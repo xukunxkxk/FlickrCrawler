28557fe4e39b4a62a852b78e0f58d7dacc9e2516
xukunxkxk <xukunxkxk@gmail.com>
2017-02-25 12:54:16 +0800

    apiAdded

---
+++ mylog/loggingTest.py
@@ -0,0 +1,10 @@
+# __author__=xk
+# -*- coding: utf-8 -*-
+# @Time    : 2017/2/24 0:02
+# @File    : loggingTest.py
+
+import logging
+
+if __name__ == '__main__':
+    log = logging.getLogger("class")
+    log.warning("QAQ")
