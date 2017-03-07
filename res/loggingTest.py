53f06c7765c930dd6bda3a16a3343c8b1d6246c2
xukunxkxk <xukunxkxk@gmail.com>
2017-03-07 13:11:33 +0800

    reconstruct

--- res/loggingTest.py
+++ res/loggingTest.py
@@ -5,17 +5,26 @@
 
 if __name__ == '__main__':
     import logging
+    import logging.config
 
-    logging.basicConfig(level=logging.DEBUG,
-                        format='%(asctime)s %(filename)s[line:%(lineno)d] %(levelname)s %(message)s' ,
-                        datefmt='%a, %d %b %Y %H:%M:%S',
-                        filename='myapp.log',
-                        filemode='w')
-    console = logging.StreamHandler()
-    console.setLevel(logging.DEBUG)
-    formatter = logging.Formatter('%(asctime)s %(filename)s[line:%(lineno)d] %(levelname)s %(message)s')
-    console.setFormatter(formatter)
-    logging.getLogger('').addHandler(console)
-    logging.debug('This is debug message')
-    logging.info('This is info message')
-    logging.warning('This is warning message')
+    # logging.basicConfig(level=logging.DEBUG,
+    #                     format='%(asctime)s %(filename)s[line:%(lineno)d] %(levelname)s %(message)s' ,
+    #                     datefmt='%a, %d %b %Y %H:%M:%S',
+    #                     filename='myapp.log',
+    #                     filemode='w')
+    # console = logging.StreamHandler()
+    # console.setLevel(logging.DEBUG)
+    # formatter = logging.Formatter('%(asctime)s %(filename)s[line:%(lineno)d] %(levelname)s %(message)s')
+    # console.setFormatter(formatter)
+    # logging.getLogger('').addHandler(console)
+    # logging.debug('This is debug message')
+    # logging.info('This is info message')
+    # logging.warning('This is warning message')
+
+
+    logging.config.fileConfig("logging.conf")
+    logger = logging.getLogger("example01")
+
+    logger.debug('This is debug message')
+    logger.info('This is info message')
+    logger.warning('This is warning message')
